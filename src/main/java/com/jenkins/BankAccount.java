6435f12db9fc584b7492345b797a80f601d502a1
Sumit Prakash <sumit.prak@gmail.com>
2018-01-24 14:22:35 +1100

    First Commit

---
+++ src/main/java/com/jenkins/BankAccount.java
@@ -0,0 +1,21 @@
+package com.jenkins;
+
+public class BankAccount 
+{
+
+  private double balance;
+
+  public BankAccount(double balance) {
+    this.balance = balance;
+  }
+
+  public double debit(double amount) {
+    if (balance < amount) {
+      amount = balance;
+    }
+
+    balance -= amount;
+    return amount;
+  }
+
+}
