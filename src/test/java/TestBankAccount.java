88a95d6bfcfbdfc2bdb712658ef0f4b591344f75
Sumit Prakash <sumit.prak@gmail.com>
2018-01-25 14:50:30 +1100

    Added Test case

--- src/test/java/TestBankAccount.java
+++ src/test/java/TestBankAccount.java
@@ -1,12 +1,19 @@
-import org.junit.Before;
+
+
+import org.junit.Assert;
 import org.junit.Test;
 
+import com.jenkins.BankAccount;
 
-public class TestBankAccount 
-{
 
-	public TestBankAccount() 
+public class TestBankAccount {
+
+	@Test
+	public void testDebitWithSufficientFunds() 
 	{
-	}
+		BankAccount account = new BankAccount (10);
+		double amount = account.debit(5);
+		Assert.assertEquals(5.0, amount);
 
+	}
 }
