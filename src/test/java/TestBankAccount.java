8289a92d458eb6c6f3b18a5b00511eee6cb334b6
Sumit Prakash <sumit.prak@gmail.com>
2018-01-25 14:59:31 +1100

    Edited "Assert" error

--- src/test/java/TestBankAccount.java
+++ src/test/java/TestBankAccount.java
@@ -14,7 +14,7 @@ public class TestBankAccount
 	{
 		BankAccount account = new BankAccount (10);
 		double amount = account.debit(5);
-		Assert.assertEquals(5.0, amount);
+		Assert.assertEquals(5.0, amount, 0);
 
 	}
 }
